531305405a9943b9faf9230ca2cc888aa6b50a81
MarcelinoV <marcelinomv98x@gmail.com>
2021-07-06 11:15:48 -0400

    Created Forms

--- Lab_6_Project/my_site/urls.py
+++ Lab_6_Project/my_site/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from polls import views
+#from polls import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /polls/
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ Lab_6_Project/polls/list_comp.py
@@ -0,0 +1,7 @@
+movies = ['The Godfather', 'Oculus',
+         'Mortal Kombat', 'American Gangster', 
+         'The Upside']
+
+joined_string = '\n'.join(movies)
+
+print(joined_string)
